3250fdb0cab72fd7efe7c02c4c185fc8edf7ac77
jake-freeman <jfreeman@hawk.iit.edu>
2016-05-06 09:45:53 -0500

    Adjust report max height

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,7 @@
 
 function resize_report() {
   if ($(window).height() < 1200) {
-    $('#report-doc > iframe').css('height', 4000 + 'px');
+    $('#report-doc > iframe').css('height', 5500 + 'px');
   }
   else {
     $('#report-doc > iframe').css('height', ($(window).height() - $('#title').height() - 65) + 'px');
